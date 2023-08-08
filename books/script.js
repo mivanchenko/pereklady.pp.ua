4d42bd06985926c3915eae1dc0ed8dfaf1f8e9a8
Michael Ivanchenko <ivanchenko.michael@gmail.com>
2023-08-08 20:01:14 +0300

    Spellchecker refactor

--- books/script.js
+++ books/script.js
@@ -5,28 +5,63 @@
     return;
   }
 
-//  let haystack = document.body.textContent;
-  let haystack = 'У першу чергу я зайшов сюди, це у першу чергу, а в першу чергу ні, у першу чергу.';
+  let haystack = document.body.textContent;
+//  let haystack = 'У першу чергу я зайшов сюди, це у першу чергу, а в першу чергу ні, у першу чергу.';
 
-  let mistakesFound = false;
+  let firstFinding = true;
   let mistakes = await getMistakes('/api/mistakes.json');
   for (let mistake of mistakes) {
-    if (!mistake || !mistake.wrong || !mistake.right) {
+    if (!isMistakeEligible(mistake)) {
       continue;
     }
-    if (!mistake.wrong.copy || !mistake.right.length) {
-      continue;
-    }
-    let needle = mistake.wrong.copy.replace(/\s+/g, '\\s+');
-    let needleRegexp = new RegExp(needle, 'gi');
-    let matches = haystack.match(needleRegexp);
+    let matches = findMistake(mistake, haystack);
     if (!matches) {
       continue;
     }
-    if (!mistakesFound) {
+    if (firstFinding) {
+      firstFinding = false;
       logg('\u2718 Mistakes found:');
-      mistakesFound = true;
     }
+    showStats(mistake, matches);
+  }
+
+  if (firstFinding) {
+    logg('\u2714 No mistakes found');
+  }
+  logg('done');
+
+  async function getMistakes(file) {
+    let myObject = await fetch(file);
+    let mistakes = await myObject.json();
+    return mistakes;
+  }
+
+  function isMistakeEligible(mistake) {
+    if (!mistake || !mistake.wrong || !mistake.right) {
+      return false;
+    }
+    if (!mistake.wrong.copy || !mistake.right.length) {
+      return false;
+    }
+    return true;
+  }
+
+  function findMistake(mistake, haystack) {
+    let needleRegexp = prepareNeedle(mistake);
+    let matches = haystack.match(needleRegexp);
+    if (!matches) {
+      return false;
+    }
+    return matches;
+  }
+
+  function prepareNeedle(mistake) {
+      let needle = mistake.wrong.copy.replace(/\s+/g, '\\s+');
+      let needleRegexp = new RegExp(needle, 'gi');
+      return needleRegexp;
+  }
+
+  function showStats(mistake, matches) {
     let allMatches = {};
     for (let match of matches) {
       allMatches[match] ||= 0;
@@ -36,18 +71,6 @@
     for (let uniqueMatch of uniqueMatches) {
       logg(`(${allMatches[uniqueMatch]}) [${uniqueMatch}]: ${mistake.right.join(', ')}`);
     }
-  }
-
-  if (!mistakesFound) {
-    logg('\u2714 No mistakes are found');
-  } else {
-    logg('done');
-  }
-
-  async function getMistakes(file) {
-    let myObject = await fetch(file);
-    let mistakes = await myObject.json();
-    return mistakes;
   }
 
   function logg(msg) {
