d9b2e2dca188991adf3e41bb3f910bf9ba27a229
Michael Ivanchenko <mivanchenko@users.noreply.github.com>
2023-08-20 14:30:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let mistakes = await getMistakes('/api/mistakes-test.json');
-console.log(mistakes);
-async function getMistakes(file) {
-  let myObject = await fetch(file);
-  let mistakes = await myObject.json();
-  return mistakes;
-}
+(async function() {
+  let mistakes = await getMistakes('/api/mistakes-test.json');
+  console.log(mistakes);
+  async function getMistakes(file) {
+    let myObject = await fetch(file);
+    let mistakes = await myObject.json();
+    return mistakes;
+  }
+})();
