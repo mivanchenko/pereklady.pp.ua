b5830e6609e6ffedada28dc808353ffcb8c3b2a4
Michael Ivanchenko <mivanchenko@users.noreply.github.com>
2023-08-20 14:02:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-console.log('ok');
-(async function() {
-  let mistakes = await getMistakes('https://pereklady.pp.ua/api/mistakes-test.json');
-  logg(mistakes);
+// console.log('ok');
+// (async function() {
+//   let mistakes = await getMistakes('https://pereklady.pp.ua/api/mistakes-test.json');
+//   logg(mistakes);
 
-  async function getMistakes(file) {
-    let myObject = await fetch(file);
-    let mistakes = await myObject.json();
-    return mistakes;
-  }
+//   async function getMistakes(file) {
+//     let myObject = await fetch(file);
+//     let mistakes = await myObject.json();
+//     return mistakes;
+//   }
 
-  function logg(msg) {
-    console.log(msg);
-  }
-})();
+//   function logg(msg) {
+//     console.log(msg);
+//   }
+// })();
+
+myFunc('["abc", "def"]');
