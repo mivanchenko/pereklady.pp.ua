7f3e8d85a052b10707ad7e57e18f9d341c560368
Michael Ivanchenko <mivanchenko@users.noreply.github.com>
2023-08-20 15:28:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 (async function() {
+  let params = new URL(document.location).searchParams;
+  if (params.get('get') !== 'mistakes') {
+    return;
+  }
   let myObject = await fetch('https://pereklady.pp.ua/api/mistakes-test.json');
   let mistakes = await myObject.json();
   jsonp(JSON.stringify(mistakes));
