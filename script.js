9a6572dc110655f11c8a5cd393cbb483226596d5
Michael Ivanchenko <mivanchenko@users.noreply.github.com>
2023-08-20 14:40:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 (async function() {
   let myObject = await fetch('https://pereklady.pp.ua/api/mistakes-test.json');
   let mistakes = await myObject.json();
+  console.log(mistakes);
   jsonp(mistakes);
 })();
