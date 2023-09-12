1173aca54ed5eeb366bd0b7fbda3134028fc83a4
Michael Ivanchenko <mivanchenko@users.noreply.github.com>
2023-09-12 14:46:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 (async function() {
   try {
+    const myHeaders = new Headers();
+    myHeaders.append('apikey', 'temp123');
     const requestOptions = {
       method: 'GET',
-      headers: new Headers(),
+      headers: myHeaders,
       redirect: 'follow',
     };
     const response = await fetch('https://pereklady.pp.ua/rules/test.json', requestOptions);
