bab954620c9f116577ffb047d6e9a4384b8ec600
Michael Ivanchenko <mivanchenko@users.noreply.github.com>
2023-09-12 14:41:59 +0300

    Update script.js
    Handle errors when fetching rules

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 (async function() {
-  let myObject = await fetch('https://pereklady.pp.ua/rules/test.json');
-  let mistakes = await myObject.json();
-  jsonp(JSON.stringify(mistakes));
+  try {
+    let response = await fetch('https://pereklady.pp.ua/rules/test.json');
+    if (!response.ok) {
+      throw new Error('Cannot fetch rules');
+    }
+    let rules = await response.json();
+    jsonp(JSON.stringify(rules));
+  } catch (err) {
+    console.error('Something went wrong when fetching rules');
+    console.error(err);
+  }
 })();
