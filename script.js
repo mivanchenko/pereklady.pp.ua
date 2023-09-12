d64f8434e086858ae466c042224d88d92138ab35
Michael Ivanchenko <mivanchenko@users.noreply.github.com>
2023-09-12 14:45:09 +0300

    Update script.js
    Add request options

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 (async function() {
   try {
-    let response = await fetch('https://pereklady.pp.ua/rules/test.json');
+    const requestOptions = {
+      method: 'GET',
+      headers: new Headers(),
+      redirect: 'follow',
+    };
+    const response = await fetch('https://pereklady.pp.ua/rules/test.json', requestOptions);
     if (!response.ok) {
       throw new Error('Cannot fetch rules');
     }
-    let rules = await response.json();
+    const rules = await response.json();
     jsonp(JSON.stringify(rules));
   } catch (err) {
     console.error('Something went wrong when fetching rules');
